68f0ff2018cd1193dd1e5f9a4b863d4d3e64fe90
jfernando0911 <jfernando0911@gmail.com>
2020-05-02 23:13:58 -0500

    connecting a device, getting heart rate measurement, and rendering info

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
 
 
 export default (props) => {
 
+    const [heartRate, setheartRate] = useState(0);
+    const [bluetoothMessage, setbluetoothMessage] = useState("");
     // let getItem = localStorage.getItem();
     // const [darkMode, setdarkMode] = useState(JSON.parse(localStorage.getItem("darkModeValue")) || false);
 
@@ -21,6 +23,82 @@ export default (props) => {
     // }
 
 
+    async function connectBluetooth() {
+        let options = {
+            filters: [
+                { services: ['heart_rate'] }
+            ]
+        }
+
+        try {
+            let device = await navigator.bluetooth.requestDevice(options);
+            console.log("Device requested: ", device);
+            setbluetoothMessage("Requesting device...");
+            let server = await device.gatt.connect();
+            console.log("Connected to Gatt Server", server);
+            setbluetoothMessage("Connecting to the Gatt Server...")
+            let service = await server.getPrimaryService('heart_rate');
+            console.log("Get heart rate service: ", service);
+            setbluetoothMessage("Getting the heart rate service...")
+            let characteristic = await service.getCharacteristic("heart_rate_measurement");
+            setbluetoothMessage("Getting the heart rate measurement...");
+            console.log(characteristic);
+            let notifications = await characteristic.startNotifications();
+            if(notifications){
+                setbluetoothMessage("");
+            }
+
+            console.log(notifications);
+            notifications.addEventListener("characteristicvaluechanged", handleNotifications);
+        } catch (error) {
+            console.log("This error: ", error);
+        }
+    }
+
+
+    function handleNotifications(e) {
+        console.log(parseHeartRate(e.currentTarget.value).heartRate);
+        setheartRate(parseHeartRate(e.currentTarget.value).heartRate);
+
+    }
+
+
+
+    function parseHeartRate(data) {
+        const flags = data.getUint8(0);
+        const rate16Bits = flags & 0x1;
+        const result = {};
+        let index = 1;
+        if (rate16Bits) {
+            result.heartRate = data.getUint16(index, /*littleEndian=*/true);
+            index += 2;
+        } else {
+            result.heartRate = data.getUint8(index);
+            index += 1;
+        }
+        const contactDetected = flags & 0x2;
+        const contactSensorPresent = flags & 0x4;
+        if (contactSensorPresent) {
+            result.contactDetected = !!contactDetected;
+        }
+        const energyPresent = flags & 0x8;
+        if (energyPresent) {
+            result.energyExpended = data.getUint16(index, /*littleEndian=*/true);
+            index += 2;
+        }
+        const rrIntervalPresent = flags & 0x10;
+        if (rrIntervalPresent) {
+            const rrIntervals = [];
+            for (; index + 1 < data.byteLength; index += 2) {
+                rrIntervals.push(data.getUint16(index, /*littleEndian=*/true));
+            }
+            result.rrIntervals = rrIntervals;
+        }
+        return result;
+    }
+
+
+
 
     return (
         <header>
@@ -28,9 +106,12 @@ export default (props) => {
             <button onClick={changeDarkMode}>ChangeMode</button>
             <button onClick={getValue}>Get a real value</button> */}
             <h1>Heart Rate Header</h1>
+            <button onClick={connectBluetooth}>Bluetooth</button>
+            <p style={{ fontSize: 60 }}>{heartRate}</p>
+            <p>{bluetoothMessage}</p>
 
         </header>
     );
 }
 
- 
+

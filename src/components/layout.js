6b99ba98ae9b9e66cdb9f9d61c658875cbaa8771
jfernando0911 <jfernando0911@gmail.com>
2020-05-05 07:58:30 -0500

    commented some code and added resets to css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/header';
-
+import styles from '../components/layout.module.scss'
 
 export default ({ children }) => {
   
@@ -8,8 +8,8 @@ export default ({ children }) => {
 
   return(
   
-    <div /*className={darkMode ? styles.darkMode : styles.lightMode}*/>
-      <h1>Heart Rate:</h1>
+    <div /*className={darkMode ? styles.darkMode : styles.lightMode}*/ className={styles.container}>
+      {/* <h1>Heart Rate:</h1> */}
       <Header /*saludo={getValue} *//>
       {children}
      

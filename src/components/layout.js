92cabcafc9c5faf07556f6ad1bc59bc7ebb2daa1
jfernando0911 <jfernando0911@gmail.com>
2020-05-10 04:31:47 -0500

    removed old code

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,54 +1,20 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import Header from '../components/header';
 import styles from '../components/layout.module.scss'
 
 
 export default ({ children }) => {
 
-  const [minHR, setminHR] = useState(0);
-  const [maxHR, setmaxHR] = useState(0);
-  const [errorMessageHR, seterrorMessageHR] = useState("");
-
-  const refMinHr = useRef();
-  const refMaxHr = useRef();
-  // refMinHr.current.value= 30;
-  // refMaxHr.current.value= 40;
-
-
-  const saveHrValues = () => {
-    console.log("This is the min heart rate value:", refMinHr.current.value);
-    console.log("This is the max heart rate value:", refMaxHr.current.value);
-    console.log(refMinHr.current);
-    console.log(refMaxHr.current);
-
-    if (refMinHr.current.value > refMaxHr.current.value) {
-      seterrorMessageHR("Your min heart rate value cant be less than your high heart rate value, please enter new values...");
-
-    } else {
-      setminHR(refMinHr.current.value);
-      setmaxHR(refMaxHr.current.value);
-      seterrorMessageHR("");
-    }
-  }
-
-
+ 
 
 
   return (
 
     <div /*className={darkMode ? styles.darkMode : styles.lightMode}*/ className={styles.container}>
-      {/* <h1>Heart Rate:</h1> */}
-      <Header /*saludo={getValue} */ minHrValue={minHR} maxHrValue={maxHR} />
+     
+      <Header/>
       {children}
-      <div className={styles.heartRateTemporalContainer}>
-        <p>Min HR</p>
-        <input type="number" min={30} max={240} mobile="auto" ref={refMinHr} />
-        <p>Max HR</p>
-        <input type="number" min={30} max={240} mobile="auto" ref={refMaxHr} />
-        <p>{errorMessageHR}</p>
-        <button onClick={saveHrValues}>Save</button>
-
-      </div>
+     
     </div>
   )
 }

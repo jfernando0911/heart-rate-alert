8a8c1e6ddaf224b34c42a74538cb9325d1b7f185
jfernando0911 <jfernando0911@gmail.com>
2020-05-10 04:32:33 -0500

    settings page created

---
+++ src/pages/settings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/navbar';
+import SettingsContainer from '../components/settingsContainer';
+
+const settings = () => {
+    return (
+        <div>
+            <Navbar/>
+            <SettingsContainer/>
+        </div>
+    );
+}
+
+export default settings;

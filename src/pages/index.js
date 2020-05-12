8cbdeec7172bbe10e15d62b5c95bd27a8881691d
jfernando0911 <jfernando0911@gmail.com>
2020-05-12 04:38:28 -0500

    navbar import was commented

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from '../components/layout'
 import { Helmet } from "react-helmet"
-import Navbar from '../components/navbar';
+// import Navbar from '../components/navbar';
 
 export default () => {
 
@@ -11,13 +11,13 @@ export default () => {
     return (
         // Detect when someone inputs data, before showing up the heartbeats and rages.
         <div>
-            <Navbar/>
+           
 
             <Helmet>
                 <meta charSet="utf-8" />
                 
                 <title>Heart Rate Alert</title>
-                <link rel="canonical" href="https://calambrias.surge.sh/" />
+                <link rel="canonical" href="https://heart-rate-alert.surge.sh/" />
                 <html lang="en" />
             </Helmet>
 
